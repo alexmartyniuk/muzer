3f9982d0ea8ef03fa1a2a6acaded2d15c33c949b
Oleksandr Martinyuk <alexander.martinyuk@gmail.com>
2018-05-13 20:13:29 +0300

    #2 Create solution and projects for Web API

---
+++ api.net/controllers/App_Start/WebApiConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Newtonsoft.Json.Serialization;
+
+namespace Controllers
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            var json = GlobalConfiguration.Configuration.Formatters.JsonFormatter;
+            json.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+        }
+    }
+}
