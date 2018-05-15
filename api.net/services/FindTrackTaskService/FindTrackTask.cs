3da4c002156daa62e1cac83f67ab6dff12256e41
Oleksandr Martinyuk <alexander.martinyuk@gmail.com>
2018-05-15 23:01:05 +0300

    #4: Created message queue service (need to be checked)

---
+++ api.net/services/FindTrackTaskService/FindTrackTask.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MuzerAPI.Implementation
+{
+    public class FindTrackTask
+    {
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public string Track { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((FindTrackTask) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = (Artist != null ? StringComparer.CurrentCulture.GetHashCode(Artist) : 0);
+                hashCode = (hashCode * 397) ^ (Album != null ? StringComparer.CurrentCulture.GetHashCode(Album) : 0);
+                hashCode = (hashCode * 397) ^ (Track != null ? StringComparer.CurrentCulture.GetHashCode(Track) : 0);
+                return hashCode;
+            }
+        }
+
+        protected bool Equals(FindTrackTask other)
+        {
+            return string.Equals(Artist, other.Artist, StringComparison.CurrentCulture) &&
+                   string.Equals(Album, other.Album, StringComparison.CurrentCulture) &&
+                   string.Equals(Track, other.Track, StringComparison.CurrentCulture);
+        }
+    }
+}
---
+++ api.net/services/FindTrackTaskService/FindTrackTaskService.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Messaging;
+using MuzerAPI.Implementation;
+
+namespace MuzerAPI
+{
+    public class FindTrackTaskService
+    {
+        private const string MessageQueueName = @".\Private$\MuzerFindTrackQueue";
+        private Dictionary<FindTrackTask, MessageQueueTransaction> _openedTransactions = new Dictionary<FindTrackTask, MessageQueueTransaction>();
+
+        private MessageQueue GetMessageQueue()
+        {
+            MessageQueue result;
+            if (!MessageQueue.Exists(MessageQueueName))
+            {
+                result = MessageQueue.Create(MessageQueueName, true);
+            }
+            else
+            {
+                result = new MessageQueue(MessageQueueName);
+            }
+
+            result.Formatter = new XmlMessageFormatter(new[] { typeof(FindTrackTask) });
+
+            return result;
+        }
+
+        public bool IsTaskExists(FindTrackTask task)
+        {
+            MessageQueue messageQueue = GetMessageQueue();
+            try
+            {
+                Message[] allMessages = messageQueue.GetAllMessages();
+                foreach (Message msg in allMessages)
+                {
+                    FindTrackTask currentTask = (FindTrackTask) msg.Body;
+                    if (task.Equals(currentTask))
+                    {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+            finally
+            {
+                messageQueue.Close();
+            }
+        }
+
+        public bool AddTask(FindTrackTask task)
+        {
+            if (IsTaskExists(task))
+            {
+                return false;
+            }
+
+            MessageQueue messageQueue = GetMessageQueue();
+            try
+            {
+                var transaction = new MessageQueueTransaction();
+                transaction.Begin();
+                try
+                {
+                    messageQueue.Send(task, transaction);
+                    transaction.Commit();
+                }
+                catch (Exception)
+                {
+                    transaction.Abort();
+                    throw;
+                }
+
+            }
+            finally
+            {
+                messageQueue.Close();
+            }
+
+            return true;
+        }
+
+        public FindTrackTask GetNextTask()
+        {
+            MessageQueue messageQueue = GetMessageQueue();
+            try
+            {
+                var transaction = new MessageQueueTransaction();
+                transaction.Begin();
+                FindTrackTask task = (FindTrackTask)messageQueue.Receive(transaction).Body;
+                _openedTransactions.Add(task, transaction);
+                return task;
+            }
+            finally
+            {
+                messageQueue.Close();
+            }
+        }
+
+        public void TaskDone(FindTrackTask task)
+        {
+            if (_openedTransactions.TryGetValue(task, out var transaction))
+            {
+                transaction.Commit();
+                _openedTransactions.Remove(task);
+            }
+            else
+            {
+                throw new Exception($"Transaction for {task} not found.");
+            }
+        }
+
+        public void TaskFailed(FindTrackTask task)
+        {
+            if (_openedTransactions.TryGetValue(task, out var transaction))
+            {
+                transaction.Abort();
+                _openedTransactions.Remove(task);
+            }
+            else
+            {
+                throw new Exception($"Transaction for {task} not found.");
+            }
+        }
+
+        public int TasksCount()
+        {
+            MessageQueue messageQueue = GetMessageQueue();
+            try
+            {
+                Message[] allMessages = messageQueue.GetAllMessages();
+                
+                return allMessages.Length;
+            }
+            finally
+            {
+                messageQueue.Close();
+            }
+        }
+    }
+}
